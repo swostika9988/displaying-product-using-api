29ac266e8e25e8fd41dde4bf52d1b97be3fb7aa4
swostika9988 <sostikamahato40@gmail.com>
2023-11-05 16:21:25 +0545

    shopping

--- src/App.js
+++ src/App.js
@@ -5,36 +5,63 @@ import { FaShoppingCart, FaStar } from "react-icons/fa";
 
 
 function App() {
+const [products, setProducts] = useState([])
+useEffect(() =>{
+  axios.get("https://fakestoreapi.com/products  ")
+  .then(res=>{
+    setProducts(res.data);
+      console.log(res.data)
+    });
+},[]);
+
   return (
     <div>
+      {/* <ul>
+        {products.map((product) =>(
+            <div style={{margin:"50px"}}>
+              <li>Id:{product.id}</li>
+              <li>Title:{product.title}</li>
+              <li>Price:{product.price}</li>
+              <li>Category:{product.category}</li>
+              <li>Description:{product.description}</li>
+              <img src={product.image} style={{height:"100px"}}/>
+            </div>
+            ))}
+          </ul> */}
       <div className="w-full p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-          <div
-            className="bg-white rounded-lg shadow p-4 transition-all  duration-300 hover:-translate-y-1 hover:shadow-lg relative"
-          >
-            <img
-              src="https://static8.depositphotos.com/1559986/1051/i/450/depositphotos_10513998-stock-photo-red-jacket.jpg"
-              className="w-full object-cover mb-4"
-            />
+      {products.map(product=>(
+        <div
+        className="bg-white rounded-lg  shadow p-4 transition-all  duration-300 hover:-translate-y-1 hover:shadow-lg relative"
+      >
+        <img
+          src={product.image}
+          className="w-full object-cover h-80 mb-4"
+        />
 
-            <div className="flex justify-between items-center pb-2">
-              <p className="bg-pink-100 px-1 text-pink-500 rounded">
-                Clothing
-              </p>
-              <p className="text-gray-600">Rs. 500</p>
-            </div>
+        <div className="flex justify-between items-center pb-2">
+          <p className="bg-pink-100 px-1 text-pink-500 rounded">
+            {product.category}
+          </p>
+          <p className="text-gray-600">{product.price}</p>
+        </div>
 
-            <p className="font-bold text-gray-800">Jacket</p>
+        <p className="font-bold text-gray-800">{product.title}</p>
 
-            <div className="flex justify-between items-center">
-              <div className="flex items-center border border-yellow-500 rounded px-1 mt-2">
-                <p className="text-yellow-500">5.0</p>
-                <FaStar className="text-yellow-500 ml-1" />
-              </div>
-              <button className={`px-4 py-2 rounded-full text-white bg-pink-500 hover:bg-pink-600 `}>
-                <FaShoppingCart />
-              </button>
-            </div>
+        <div className="flex justify-between items-center">
+          <div className="flex items-center border border-yellow-500 rounded px-1 mt-2">
+            <p className="text-yellow-500">{product.rating.rate}/</p>
+            <p className="text-yellow-500">{product.rating.count}</p>
+            <FaStar className="text-yellow-500 ml-1" />
           </div>
+          <button className={`px-4 py-2 rounded-full text-white bg-pink-500 hover:bg-pink-600 `}>
+            <FaShoppingCart />
+          </button>
+        </div>
+      </div>
+      ))}
+          
+          
+          
       </div>
 
     </div>
